318ecb39d993bcdd7d3118176f95773e7b26303e
rkapoor10 <ritik.kapoor@harness.io>
2023-05-11 11:42:25 +0530

    encapsulation

---
+++ Encapsulation/Encapsulation.java
@@ -0,0 +1,10 @@
+
+class Human{
+
+}
+
+public class Encapsulation {
+    public static void main(String a[]){
+        System.out.println("Hello Encapsulation");
+    }
+}
---
+++ Hello.java
@@ -0,0 +1,21 @@
+class Calculator{
+    int a; //this is instance variable
+    public int add(int no1, int no2) // variables no1 and no2 are local vars
+    {
+        int sum = no1 + no2; 
+        return sum;
+    }
+
+    public double add(int no1){ // method overloading
+        return 0.0;
+    }
+}
+
+public class Hello{
+    public static void main(String a[]) {
+        
+        Calculator calc = new Calculator();
+        double result = calc.add(4);
+        System.out.println(result);
+    }
+}
