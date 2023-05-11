2cda3018f95dc2ea34e6120a0abdf8accadb0b5a
rkapoor10 <ritik.kapoor@harness.io>
2023-05-11 12:37:34 +0530

    getters and setters

--- Encapsulation/Encapsulation.java
+++ Encapsulation/Encapsulation.java
@@ -1,10 +1,40 @@
-
+// package Encapsulation;
 class Human{
+    private int age;
+   
+    private String name;
 
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    
 }
 
 public class Encapsulation {
     public static void main(String a[]){
+
+        Human obj = new Human();
+
         System.out.println("Hello Encapsulation");
+
+        // System.out.println(obj.age); // this won't be accessible directly
+        System.out.println(obj.getAge());
+
+        obj.setAge(5);
+
+        System.out.println(obj.getAge());
+
+
     }
 }
