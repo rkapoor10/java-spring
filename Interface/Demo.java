1485ba2b55725a0e05bd8459fb531d21e6713cb1
rkapoor10 <ritik.kapoor@harness.io>
2023-05-16 18:15:04 +0530

    some more of interface

--- Interface/Demo.java
+++ Interface/Demo.java
@@ -1,10 +1,18 @@
+import java.security.PublicKey;
+
 interface A 
 {
-    int age = 23;
+    int age = 23; //final and static
     String area = "Mumbai";
 
-    void show();
+    void show(); // abstract and public
     void config();
+}
+
+
+interface X
+{
+    void fromX();
 }
 
 class B implements A
@@ -19,6 +27,28 @@ class B implements A
 }
 
 
+//we can also implement multiple interfaces unlike classes which can 
+// only extend a single class
+
+class C implements A,X
+{
+    public void show(){
+        System.out.println("This is show");
+    }
+
+    public void config(){
+        System.out.println("This is config");
+    }
+
+    public void fromX(){
+        System.out.println("This is from X interface");
+    }
+}
+
+
+
+
+
 public class Demo {
     public static void main(String a[])
     {
@@ -29,6 +59,28 @@ public class Demo {
         // class names
 
         System.out.println("age : "+A.age+" "+"Area: "+A.area) ;
+
+
+        A obj1 = new C();
+        obj1.show();
+
+        /**
+         * Also to implement the methods, the reference should be of that interface
+         * as, obj cannot run method fromX()
+         */
+
+        //  obj.fromX(); // undefined for type A
+
+        X obj2 = new C();
+        obj2.fromX();  // this will work 
+
+        C obj3 = new C(); // changing the reference to C to implement both methods from same object
+
+        obj3.show();
+        obj3.fromX();
+
+
+
 
     }    
 }
